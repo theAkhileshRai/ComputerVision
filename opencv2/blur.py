546490461cd7cfe51e8c5c4b91029060f250072d
Akhilesh Rai <arsrocz@gmail.com>
2020-06-10 22:19:26 +0530

    Create blur.py

---
+++ opencv2/blur.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+img = cv2.imread('/Users/manishrai/PycharmProjects/OpenCV/220px-Lenna_(test_image).png',0)
+
+kernel = np.ones((5,5),np.float32)/25
+
+dst = cv2.blur(img,(5,5))
+
+plt.subplot(121)
+plt.imshow(img),plt.title('Original')
+plt.xticks([])
+plt.yticks([])
+plt.subplot(122)
+plt.imshow(dst)
+plt.title('Blur')
+plt.show()
